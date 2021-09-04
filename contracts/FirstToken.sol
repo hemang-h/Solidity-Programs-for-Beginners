61e83494e042f36e64573acf3ceae1a357479fe8
hemang-joshi <73999607+hemang-joshi@users.noreply.github.com>
2021-09-04 13:16:33 +0530

    Commited together

---
+++ contracts/FirstToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4 ;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract FirstToken {
+
+    using SafeMath for uint256;
+    // string constant public name = "Nunmun Crypto Currency ";
+    // string constant public symbol = "BD";
+    uint256 public totalSupply = 10000;
+    uint256 public _limitSupply = (totalSupply/10);
+    address private admin;
+    
+    constructor () ERC20 ("TimeLock", "TimeLock"){
+      
+    }
+  }   
