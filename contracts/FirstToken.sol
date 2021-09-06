407f9cba6377129486f61e5c3257a2ab51c75705
hemang-joshi <73999607+hemang-joshi@users.noreply.github.com>
2021-09-06 13:22:55 +0530

    Second commit

--- contracts/FirstToken.sol
+++ contracts/FirstToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4 ;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-
+?????????????????????????????????/I am a dote
 contract FirstToken {
 
     using SafeMath for uint256;
