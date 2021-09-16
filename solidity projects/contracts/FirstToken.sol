101de2f6116e72245a37118381417d7ce4a07e36
hemang-joshi <73999607+hemang-joshi@users.noreply.github.com>
2021-09-16 08:49:50 +0530

    committed

--- solidity projects/contracts/FirstToken.sol
+++ solidity projects/contracts/FirstToken.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4 ;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract FirstToken is ERC20{
+contract FirstToken {
 
     using SafeMath for uint256;
     // string constant public name = "Nunmun Crypto Currency ";
@@ -14,8 +14,8 @@ contract FirstToken is ERC20{
     uint256 public _limitSupply = (totalSupply/10);
     address private admin;
     
+    
     constructor () ERC20 ("TimeLock", "TimeLock"){
-      admin = msg.sender;
-      _limitSupply = block.timestamp;
+      
     }
   }   
